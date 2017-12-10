3e3d3647f8ddb450f2761bb9e9ce07d249f47203
Nenivar <nenivar@gmail.com>
2017-12-10 19:16:02 +0000

    Stage 1 complete

--- sketch.c
+++ sketch.c
@@ -9,8 +9,6 @@
  *  STRUCTURES
  */
 
-const int MAX_INSTRUCTIONS = 100;
-
 enum OPCODE {
     DX, DY, DT, PEN
 };
@@ -37,6 +35,8 @@ typedef struct state state;
 state *newState (display *disp) {
     state *new = malloc (sizeof (state));
     new->penDown = false;
+    new->prevX = 0;
+    new->prevY = 0;
     new->x = 0;
     new->y = 0;
     new->disp = disp;
@@ -46,15 +46,18 @@ state *newState (display *disp) {
 
 // returns the opcode for a given instruction
 OPCODE extractOpcode (unsigned char instruction) {
-    return (instruction >> 0x6) & 0x03;;
+    return (instruction >> 0x6) & 0x03;
 }
 
-// returns the operand (betwen -32 & 31) for a given instruction
+// returns the operand (between -32 & 31) for a given instruction
 char extractOperand (unsigned char instruction) {
-    char operand = instruction & 0x1F;
-    if ((instruction & 0x20) == 0x20) operand -= 32;
+    return instruction & 0x3F;
+}
 
-    return operand;
+// returns the operand (between 0 & 63) for a dt instruction
+char extractMoveOperand (unsigned char instruction) {
+    return (instruction & 0x20) == 0x20 ? (instruction & 0x1F) - 32
+                                     : (instruction & 0x1F);
 }
 
 /*
@@ -63,12 +66,16 @@ char extractOperand (unsigned char instruction) {
 
 // reads in a sketch file and returns a set of instructions
 instructionSet *readFile (char *loc) {
-    instructionSet *set = malloc (sizeof (instructionSet));
+    int i = 0, tempSize = 10;
+    instructionSet *set = malloc (sizeof (instructionSet) + tempSize);
 
     FILE *in = fopen (loc, "rb");
     unsigned char b = fgetc (in);
-    int i = 0;
     while (!feof (in)) {
+        if (i > tempSize) {
+            tempSize *= 1.5;
+            set = realloc (set, sizeof (instructionSet) + tempSize);
+        }
         set->instructions [i] = b;
         b = fgetc (in);
         i++;
@@ -84,54 +91,54 @@ instructionSet *readFile (char *loc) {
  *  OUTPUT
  */
 
-display *setupDisplay () {
-    display *d = newDisplay ("line.sketch", 200, 200);
-    //clear (d);
+// creates a new display
+display *setupDisplay (char *name) {
+    display *d = newDisplay (name, 200, 200);
 
     return d;
 }
 
+void executeDx (state *s, char instruction) {
+    char dx = extractMoveOperand (instruction);
+    s->x += dx;
+}
+
+void executeDy (state *s, unsigned char instruction) {
+    char dy = extractMoveOperand (instruction);
+    s->y += dy;
+
+    if (s->penDown) line (s->disp, s->prevX, s->prevY, s->x, s->y);
+
+    s->prevX = s->x;
+    s->prevY = s->y;
+}
+
+void executeDt (state *s, char instruction) {
+    pause (s->disp, extractOperand (instruction) * 10);
+}
+
+void executePen (state *s) {
+    s->penDown = !s->penDown;
+}
+
 void interpretInstr (state *s, unsigned char instruction) {
     OPCODE opcode = extractOpcode (instruction);
-    char operand = extractOperand (instruction);
 
     switch (opcode) {
         case DX:
-            s->prevX = s->x;
-            s->x += operand;
-
-            if (s->penDown) {
-                //if (validPrev)
-                
-            if (s->penDown && operand != 0) line (s->disp, s->prevX, s->prevY,
-                                s->x, s->y);
-            }
-
-            printf ("Moved pen to x=%d; (%d, %d) -> (%d, %d)\n", s->x, s->prevX, s->prevY, s->x, s->y);
-
+            //printf ("Moved pen to x=%d; (%d, %d) -> (%d, %d)\n", s->x, s->prevX, s->prevY, s->x, s->y);
+            executeDx (s, instruction);
             break;
         case DY:
-            s->prevY = s->y;
-            s->y += operand;
-
-            if (s->penDown && operand != 0) line (s->disp, s->prevX, s->prevY,
-                                s->x, s->y);
-
-            printf ("Moved pen to y=%d; (%d, %d) -> (%d, %d)\n", s->y, s->prevX, s->prevY, s->x, s->y);
-            
+            //printf ("Moved pen to y=%d; (%d, %d) -> (%d, %d)\n", s->y, s->prevX, s->prevY, s->x, s->y);
+            executeDy (s, instruction);
             break;
         case DT:
-
+            executeDt (s, instruction);
             break;
         case PEN:
-            
-            s->penDown = !s->penDown;
-            if (s->penDown) {
-s->prevX = s->x;
-            s->prevY = s->y;
-            }
-            
-            printf ("Changed pen status to x=%d\n", s->penDown);
+            //printf ("Changed pen status to x=%d\n", s->penDown);
+            executePen (s);
             break;
         default:
             printf ("Invalid instruction!\n");
@@ -142,7 +149,6 @@ s->prevX = s->x;
 void interpretInstrSet (state *s, instructionSet *set) {
     for (int i = 0; i < set->n; i++){
         interpretInstr (s, set->instructions [i]);
-        //pause (s->disp, 1000);
     }
     
     free (set);
@@ -165,10 +171,15 @@ void testExtCode () {
 
 void testExtAnd () {
     assert (extractOperand (0x00) == 0);
-    assert (extractOperand (0x37) == -9);
-    assert (extractOperand (0xFF) == -1);
-    assert (extractOperand (0x20) == -32);
+    assert (extractOperand (0x37) == 55);
+    assert (extractOperand (0xFF) == 63);
     assert (extractOperand (0x1F) == 31);
+
+    assert (extractMoveOperand (0x00) == 0);
+    assert (extractMoveOperand (0x37) == -9);
+    assert (extractMoveOperand (0xFF) == -1);
+    assert (extractMoveOperand (0x20) == -32);
+    assert (extractMoveOperand (0x1F) == 31);
 }
 
 void testReadFile () {
@@ -194,17 +205,20 @@ void test () {
 int main (int n, char *varg[n]) {
     test ();
 
-    display *d = setupDisplay ();
-    state *s = newState (d);
+    if (n > 1) {
+        char *file = varg [1];
 
-    // oxo, diag, cross
-    instructionSet *set = readFile ("line.sketch");
-    interpretInstrSet (s, set);
+        display *d = setupDisplay (file);
+        state *s = newState (d);
+        
+        instructionSet *set = readFile (file);
+        interpretInstrSet (s, set);
 
-    //key (d);
+        free (s);
+        free (d);
+    } else {
 
-    free (d);
-    free (s);
+    }
 
     return 1;
 }
